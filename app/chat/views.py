7fd0611eac58d003e6b1bfd16d0e70ca2a76a8cc
openanswer <mathemagicalhelper@gmail.com>
2017-06-02 19:13:34 -0400

    Updated socketio to v 2.0.2. Added a way to view ONLINE_USERS

--- app/chat/views.py
+++ app/chat/views.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import jsonify, render_template
 from flask_login import login_required, current_user
 
 from . import chat
@@ -8,3 +8,15 @@ from . import chat
 @login_required
 def chat_room(room):
     return render_template('chat/chat.html', title='(0) %s' % room.capitalize(), user=current_user, navtab='chat')
+
+
+@chat.route('/online')
+@login_required
+def online_users():
+    if current_user.is_admin:
+        from .events import ONLINE_USERS
+        return jsonify(
+            {username: {room: list(ids) for (room, ids) in rooms.items()} for (username, rooms) in ONLINE_USERS.items()}
+        )
+    else:
+        return {'online': 'Only admins can see this'}
