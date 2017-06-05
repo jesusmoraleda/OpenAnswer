478d57888ec4e9e382c87ec4a55bfc297bbe5e95
openanswer <mathemagicalhelper@gmail.com>
2017-06-05 01:35:56 -0400

    Added support for multiple chats in the same window (beta version, should have called it pre-alpha instead

--- app/chat/views.py
+++ app/chat/views.py
@@ -10,6 +10,12 @@ def chat_room(room):
     return render_template('chat/chat.html', title='(0) %s' % room.capitalize(), user=current_user, navtab='chat')
 
 
+@chat.route('/beta-chat')
+@login_required
+def beta_chat():
+    return render_template('chat/chat-beta.html', title='Chat', user=current_user)
+
+
 @chat.route('/online')
 @login_required
 def online_users():
