29d8209ddf6849c8cdcefaf8727c2e1fc22abaa7
Hedgehog <openhedgepiggy@gmail.com>
2017-07-23 13:28:11 -0400

    Cleaned up message and room template

--- app/static/js/chat/templates.js
+++ app/static/js/chat/templates.js
@@ -1,15 +1,16 @@
 function getChatRoomTemplate(roomName) {
     var chatWindowHtml =
         '<div class="chatWindow" id="' + roomName + '">' +
-        '<ul class="chatMessages">' +
-        '</ul>' +
-        '<input class="chatEntry" id="' + roomName + '" ' + 'type="text">' +
+            '<ul class="chatMessages"></ul>' +
+            '<input class="chatEntry" id="' + roomName + '" ' + 'type="text">' +
         '</div>';
     return chatWindowHtml;
 }
 
 function getMessageTemplate(msg, markdown) {
-    return '<li id="chatMessage" timestamp="' + msg.timestamp + '">' +
-        msg.username + ': ' + markdown.renderInline(msg.content) +
-        '<div id="timestamp"></div></li>'
+    var messageTemplate =
+        '<li id="chatMessage" timestamp="' + msg.timestamp + '">' +
+            msg.username + ': ' + markdown.renderInline(msg.content) +
+        '<div id="timestamp"></div></li>';
+    return messageTemplate;
 }
