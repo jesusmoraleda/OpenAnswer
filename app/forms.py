882e7c9aaee51f673bcf6d6e59c0620d6f41d60e
Hedgehog <openhedgepiggy@gmail.com>
2018-04-14 23:10:29 -0400

    added beta flag

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField
-from wtforms.validators import Length, Regexp
+from wtforms.validators import Length, Regexp, UUID, ValidationError
+import os
 
 
 class SignupForm(FlaskForm):
@@ -12,8 +13,20 @@ class SignupForm(FlaskForm):
             Length(min=3, max=16, message='Username needs to be between 3 and 16 chars long')
         ]
     )
+    if os.environ['IS_BETA']:
+        beta_key = StringField(
+            'beta-key', validators=[UUID(message='Message must be a beta-key'),
+                                    validate_beta_key]
+        )
+
 
 
 class PostForm(FlaskForm):
     content = StringField('content')
     category = StringField('category')
+
+
+def validate_beta_key(form, field):
+    with open(os.environ['BETA_KEYS_PATH']) as f:
+        if field.data not in f.readlines():
+            raise ValidationError(message='Invalid key')
