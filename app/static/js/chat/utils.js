e827d43c72537d29840880c540ae05c82c63adcb
James <ultrilliam@gmail.com>
2018-04-20 11:16:35 -0700

    Moved new code to utils

--- app/static/js/chat/utils.js
+++ app/static/js/chat/utils.js
@@ -1,3 +1,21 @@
+var ultrilliam = {
+    addYoutube: function(message) {
+        var r1 = /youtube.com\/watch.*[?&]v=(.{11})/i;
+		var r2 = /youtu.be\/(.{11})/i;
+		var isYoutube = r1.test(message) || r2.test(message);
+		if (isYoutube) {
+          var videoId = RegExp.$1;
+          var message = message + '<br/>';
+          return (message + '<iframe width="160" height="200" src="https://www.youtube.com/embed/' + videoId + '" frameborder="0" allowfullscreen></iframe>');
+		}
+		return message;
+	},
+    checkScroll: function(element) {
+        var delta = Math.floor(element[0].scrollHeight) - Math.floor(element.scrollTop());
+        return (delta - 150 < element.height()) && (delta + 150 > element.height());
+    }
+};
+
 function show_timestamp() {
     var ts = $.timeago(new Date(this.getAttribute('timestamp')));
     var ts_div = $(this).children("#timestamp");
