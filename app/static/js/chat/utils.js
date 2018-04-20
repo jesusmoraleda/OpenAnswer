1f03f05acb63b25e4d0bb31ee252b0ef22ac68b6
James <ultrilliam@gmail.com>
2018-04-20 11:23:18 -0700

    removed ultrilliam reference

--- app/static/js/chat/utils.js
+++ app/static/js/chat/utils.js
@@ -1,20 +1,19 @@
-var ultrilliam = {
-    addYoutube: function(message) {
-        var r1 = /youtube.com\/watch.*[?&]v=(.{11})/i;
-		var r2 = /youtu.be\/(.{11})/i;
-		var isYoutube = r1.test(message) || r2.test(message);
-		if (isYoutube) {
-          var videoId = RegExp.$1;
-          var message = message + '<br/>';
-          return (message + '<iframe width="160" height="200" src="https://www.youtube.com/embed/' + videoId + '" frameborder="0" allowfullscreen></iframe>');
-		}
-		return message;
-	},
-    checkScroll: function(element) {
-        var delta = Math.floor(element[0].scrollHeight) - Math.floor(element.scrollTop());
-        return (delta - 150 < element.height()) && (delta + 150 > element.height());
-    }
-};
+function addYoutube(message) {
+	var r1 = /youtube.com\/watch.*[?&]v=(.{11})/i;
+	var r2 = /youtu.be\/(.{11})/i;
+	var isYoutube = r1.test(message) || r2.test(message);
+	if (isYoutube) {
+		var videoId = RegExp.$1;
+		var message = message + '<br/>';
+		return (message + '<iframe width="160" height="200" src="https://www.youtube.com/embed/' + videoId + '" frameborder="0" allowfullscreen></iframe>');
+	}
+	return message;
+}
+
+function checkScroll(element) {
+	var delta = Math.floor(element[0].scrollHeight) - Math.floor(element.scrollTop());
+	return (delta - 150 < element.height()) && (delta + 150 > element.height());
+}
 
 function show_timestamp() {
     var ts = $.timeago(new Date(this.getAttribute('timestamp')));
@@ -38,7 +37,7 @@ function scrollChatToBottom(room, delay, forceScroll) {
         var messageContainer = getMessageContainer(room);
         if (!messageContainer.prop('pauseScroll')) {
             //forceScroll when we don't care about sizes and just want to scroll down (i.e. on first load)
-            if (forceScroll || ultrilliam.checkScroll(messageContainer)) {
+            if (forceScroll || checkScroll(messageContainer)) {
                 messageContainer.scrollTop(messageContainer.prop('scrollHeight'));
             }
         } else {
