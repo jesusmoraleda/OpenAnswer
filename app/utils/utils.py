8294a3fd1a8e20f732f63dd930d960d4aedc8d7b
Hedgehog <openhedgepiggy@gmail.com>
2018-04-28 00:34:25 -0400

    updated nginx config to rate limit requests

--- app/utils/utils.py
+++ app/utils/utils.py
@@ -2,6 +2,7 @@ from flask import request
 
 
 # Taken from https://github.com/mattupstate/flask-security/blob/f3948038ece799267597bf63b00fd02f4e6daedb/flask_security/utils.py#L64
+# FIXME check if we're dropping the IP we need in request.headers
 def get_remote_addr():
     if 'X-Forwarded-For' in request.headers:
         remote_addr = request.headers.getlist('X-Forwarded-For')[0].rpartition(' ')[-1]
