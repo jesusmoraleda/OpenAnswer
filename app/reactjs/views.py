8dafe6c90ba88d69fe3f2960999823cd59c9a389
hedgepiggy <ailecwu@email.virginia.edu>
2019-11-14 23:00:15 -0800

    #156 login required to view reactjs chat

--- app/reactjs/views.py
+++ app/reactjs/views.py
@@ -1,4 +1,5 @@
 from flask import send_from_directory
+from flask_login import login_required
 
 from . import reactjs
 
@@ -6,5 +7,6 @@ from . import reactjs
 @reactjs.route('/', defaults={'path': ''})
 # We also need to serve the static files
 @reactjs.route('/<path:path>')
+@login_required
 def serve(path):
     return send_from_directory(reactjs.static_folder, path or 'index.html')
